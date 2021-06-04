6efb9d0a4c15efb68cb4ead5635df2d8242389c3
Rodri <sergiorodrigofernandeztesta@gmail.com>
2021-06-04 14:14:39 -0400

    Cudart-corrected

--- Fruits_project/cuda-rt_fruits/fruit_test.py
+++ Fruits_project/cuda-rt_fruits/fruit_test.py
@@ -16,7 +16,7 @@ model = load_model('models/fruits_model.h5')
 #curr_img=cv2.imread('/home/ubuntu/tensorrt/URFD_images/Falls/fall_fall-02/frame0040.jpg')
 
 
-cap = cv2.VideoCapture('fruits.mp4')
+cap = cv2.VideoCapture('fruitsv2.mp4')
 #cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 prev_img=frame
@@ -30,31 +30,37 @@ while(True):
         
     ret, frame = cap.read()
     curr_img_res=cv2.resize(frame,(250,250))
-    # curr_img_c=cv2.cvtColor(curr_img_res, cv2.COLOR_BGR2GRAY)
-    # flow = cv2.calcOpticalFlowFarneback(prev_img_c,curr_img_c, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-    # vert = cv2.normalize(flow[...,1], None, 0, 255, cv2.NORM_MINMAX)
-    # vert = vert.astype('uint8')
-    
-    # smoothed = cv2.filter2D(vert,-1,kernel)
-
-    # back = cv2.cvtColor(smoothed,cv2.COLOR_GRAY2RGB)
-
-    #fall = np.expand_dims(back ,axis=0)
+    show = curr_img_res
+    curr_img_res=cv2.cvtColor(curr_img_res, cv2.COLOR_RGB2BGR)
     fall = np.expand_dims(curr_img_res ,axis=0)
     start_time = time.time()
     fall_norm = fall/255.0
     preds = model.predict(fall_norm)
     #acc=acc+(time.time()-start_time)
     if preds[0][0] < 0.5:
-        print("Blueberry",preds," FPS: ", 1.0/(time.time()-start_time))
-    else:
         print("Strawberry",preds," FPS: ", 1.0/(time.time()-start_time))
-    cv2.imshow("frame", curr_img_res)
+        cv2.putText(show, 
+                'Strawberry', 
+                (50, 50), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                (255, 0, 0), 
+                2, 
+                cv2.LINE_AA)
+    else:
+        print("Blueberry",preds," FPS: ", 1.0/(time.time()-start_time))
+        cv2.putText(show, 
+                'Blueberry', 
+                (50, 50), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                (0, 0, 255), 
+                2, 
+                cv2.LINE_AA)
+    cv2.imshow("frame", show)
     cv2.waitKey(1)
     # prev_img_c=curr_img_c
     # print(preds," FPS: ", 1.0/(time.time()-start_time))
     acc=acc+1.0/(time.time()-start_time)
     counter=counter+1
-    if counter>=1250:
+    if counter>=1249:
         print('average FPS:',acc/counter)
         break
